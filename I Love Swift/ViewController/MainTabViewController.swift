2dc298101f2fa0d7c4463e9628aff74b7ce99a60
Yi-Hua Liang <yihua1218@gmail.com>
2017-09-19 08:16:42 +0800

    Sort tab by tag.

--- I Love Swift/ViewController/MainTabViewController.swift
+++ I Love Swift/ViewController/MainTabViewController.swift
@@ -33,28 +33,32 @@ class MainTabViewController: UITabBarController, UITabBarControllerDelegate {
         tabs["News"]!["size"] = CGSize(width: 35, height: 35)
         tabs["News"]!["title"] = "News"
         tabs["News"]!["vc"] = NewsViewController()
+        tabs["News"]!["tag"] = 1
         
         tabs["Map"] = [String: Any]()
         tabs["Map"]!["icon"] = FAType.FAMap.rawValue
         tabs["Map"]!["size"] = CGSize(width: 35, height: 35)
         tabs["Map"]!["title"] = "Map"
         tabs["Map"]!["vc"] = MapViewController()
+        tabs["Map"]!["tag"] = 2
         
         tabs["Table"] = [String: Any]()
         tabs["Table"]!["icon"] = FAType.FATable.rawValue
         tabs["Table"]!["size"] = CGSize(width: 35, height: 35)
         tabs["Table"]!["title"] = "Table"
         tabs["Table"]!["vc"] = ItemTableViewController()
+        tabs["Table"]!["tag"] = 3
         
         tabs["About"] = [String: Any]()
         tabs["About"]!["icon"] = FAType.FABars.rawValue
         tabs["About"]!["size"] = CGSize(width: 35, height: 35)
         tabs["About"]!["title"] = "About"
         tabs["About"]!["vc"] = AboutViewController()
+        tabs["About"]!["tag"] = 4
         
         var viewControllers = [UIViewController]()
-        var tag = 1
         for (tab_id, tab_def) in tabs {
+            let tag = tabs[tab_id]!["tag"] as? Int
             let vc = tabs[tab_id]!["vc"] as? UIViewController
             let title = tab_def["title"]
             print("\(tab_id) => \(title ?? 0)")
@@ -67,39 +71,24 @@ class MainTabViewController: UITabBarController, UITabBarControllerDelegate {
             tabs[tab_id]!["bar_item"] = UITabBarItem(
                 title: title as? String,
                 image: tabs[tab_id]!["image"] as? UIImage,
-                tag: tag)
+                tag: tag!)
             vc?.tabBarItem = tabs[tab_id]!["bar_item"] as? UITabBarItem
-            print(vc!)
             viewControllers.append(vc!)
-            tag += 1
         }
-        self.viewControllers = viewControllers
+        
+        let result : [UIViewController] = viewControllers.sorted {
+            switch ($0.tabBarItem.tag, $1.tabBarItem.tag) {
+            case (nil, nil), (_, nil):
+                return true
+            case (nil, _):
+                return false
+            case let (lhs, rhs):
+                return lhs < rhs
+            }
+        }
+        
+        self.viewControllers = result
         print(self.viewControllers!)
-
-        /*
-        // Create Tab one
-        newsImage = UIImage.init(icon: .FANewspaperO, size: CGSize(width: 35, height: 35))
-        let tabOne = NewsViewController()
-        let tabOneBarItem = UITabBarItem(title: "News", image: newsImage, tag: 1)
-        
-        tabOne.tabBarItem = tabOneBarItem
-        
-        
-        // Create Tab two
-        tableImage = UIImage.init(icon: .FATable, size: CGSize(width: 35, height: 35))
-        let tabTwo = ItemTableViewController()
-        let tabTwoBarItem = UITabBarItem(title: "Table", image: tableImage, tag: 2)
-        
-        tabTwo.tabBarItem = tabTwoBarItem
-        
-        // Create Tab third
-        aboutImage = UIImage.init(icon: .FABars, size: CGSize(width: 35, height: 35))
-        let tabThird = AboutViewController()
-        let tabThirdBarItem = UITabBarItem(title: "About", image: aboutImage, tag: 3)
-        
-        tabThird.tabBarItem = tabThirdBarItem
-        
-        self.viewControllers = [tabOne, tabTwo, tabThird]*/
     }
     
     // UITabBarControllerDelegate method
